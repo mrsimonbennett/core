8bc98858d6aeda6c09d30e418744f1647714ba52
Simon Bennett <simon@bennett.im>
2015-06-29 20:11:45 +0100

    Clean up routes a little bit, maybe different files

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,13 +1,5 @@
 <?php
 /** @var Router $router */
-use FullRent\Core\Application\Http\Controllers\ApplicationController;
-use FullRent\Core\Application\Http\Controllers\Auth\AuthController;
-use FullRent\Core\Application\Http\Controllers\CompanyController;
-use FullRent\Core\Application\Http\Controllers\CompanyDirectDebit;
-use FullRent\Core\Application\Http\Controllers\ContractsController;
-use FullRent\Core\Application\Http\Controllers\PropertiesController;
-use FullRent\Core\Application\Http\Controllers\Tenant\ContractsController as ContractsControllerTenant;
-use FullRent\Core\Application\Http\Controllers\UserController;
 use Illuminate\Routing\Router;
 
 /**
@@ -15,12 +7,12 @@ use Illuminate\Routing\Router;
  */
 $router->group(['prefix' => 'companies'],
     function () use ($router) {
-        $router->post('', CompanyController::class . '@createCompany');
-        $router->get('exists/{domain}', CompanyController::class . '@checkExists');
-        $router->get('{domain}', CompanyController::class . '@show');
+        $router->post('', 'CompanyController@createCompany');
+        $router->get('exists/{domain}', 'CompanyController@checkExists');
+        $router->get('{domain}', 'CompanyController@show');
 
-        $router->get('{id}/direct-debit/authorization_url',CompanyDirectDebit::class . '@authorizationUrl');
-        $router->post('{id}/direct-debit/access_token',CompanyDirectDebit::class . '@accessToken');
+        $router->get('{id}/direct-debit/authorization_url', 'CompanyDirectDebit@authorizationUrl');
+        $router->post('{id}/direct-debit/access_token', 'CompanyDirectDebit@accessToken');
 
     }
 );
@@ -29,78 +21,75 @@ $router->group(['prefix' => 'companies'],
  */
 $router->group(['prefix' => 'properties'],
     function () use ($router) {
-        $router->post('accept-applications', PropertiesController::class . '@acceptApplication');
-        $router->post('close-applications', PropertiesController::class . '@closeApplication');
-        $router->get('{id}/history', PropertiesController::class . '@getHistory');
-        $router->post('', PropertiesController::class . '@listNewProperty');
-        $router->get('', PropertiesController::class . '@index');
-        $router->get('{id}', PropertiesController::class . '@show');
+        $router->post('accept-applications', 'PropertiesController@acceptApplication');
+        $router->post('close-applications', 'PropertiesController@closeApplication');
+        $router->get('{id}/history', 'PropertiesController@getHistory');
+        $router->post('', 'PropertiesController@listNewProperty');
+        $router->get('', 'PropertiesController@index');
+        $router->get('{id}', 'PropertiesController@show');
 
         /**
          * Contracts
          */
-        $router->get('{id}/contracts', ContractsController::class . '@index');
+        $router->get('{id}/contracts', 'ContractsController@index');
 
     }
 );
 $router->group(['prefix' => 'contracts/{id}'],
     function () use ($router) {
-        $router->get('', ContractsController::class . '@show');
+        $router->get('', 'ContractsController@show');
 
-        $router->post('dates', ContractsController::class . '@saveDates');
-        $router->post('rent', ContractsController::class . '@saveRent');
-        $router->post('documents', ContractsController::class . '@saveDocuments');
-        $router->post('lock', ContractsController::class . '@lockContract');
-        $router->post('landlord-sign', ContractsController::class . '@landlordSignContract');
+        $router->post('dates', 'ContractsController@saveDates');
+        $router->post('rent', 'ContractsController@saveRent');
+        $router->post('documents', 'ContractsController@saveDocuments');
+        $router->post('lock', 'ContractsController@lockContract');
+        $router->post('landlord-sign', 'ContractsController@landlordSignContract');
 
-        $router->post('tenant-upload-id', ContractsController::class . '@tenantUploadIdDocument');
-        $router->post('tenant-upload-earnings', ContractsController::class . '@tenantUploadEarningsDocument');
-        $router->post('tenant-sign-contract', ContractsController::class . '@tenantSignContract');
+        $router->post('tenant-upload-id', 'ContractsController@tenantUploadIdDocument');
+        $router->post('tenant-upload-earnings', 'ContractsController@tenantUploadEarningsDocument');
+        $router->post('tenant-sign-contract', 'ContractsController@tenantSignContract');
 
-        $router->get('deposit-information', ContractsController::class . '@getDepositInformation');
-        $router->get('deposit/{tenantId}', ContractsController::class . '@getDepositInformationForTenant');
+        $router->get('deposit-information', 'ContractsController@getDepositInformation');
+        $router->get('deposit/{tenantId}', 'ContractsController@getDepositInformationForTenant');
 
-        $router->post('tenant-pay-deposit', ContractsController::class . '@tenantPayDeposit');
-        $router->get('tenant/direct-debit/authorization_url', ContractsController::class . '@tenantAuthorizationUrl');
-        $router->post('tenant/direct-debit/access_token', ContractsController::class . '@tenantDirectDebitAccessToken');
+        $router->post('tenant-pay-deposit', 'ContractsController@tenantPayDeposit');
+        $router->get('tenant/direct-debit/authorization_url', 'ContractsController@tenantAuthorizationUrl');
+        $router->post('tenant/direct-debit/access_token', 'ContractsController@tenantDirectDebitAccessToken');
 
 
     }
 );
 
-
-$router->get('/tenants/{id}/contracts', ContractsControllerTenant::class . '@getTenantsContracts');
-
+$router->get('/tenants/{id}/contracts', 'Tenant\ContractsController@getTenantsContracts');
 
 /**
  * Users
  */
-$router->resource('users', UserController::class);
+$router->resource('users', 'UserController');
 
 /*
  * Auth
  */
 $router->group(['prefix' => 'auth'],
     function () use ($router) {
-        $router->post('login', ['uses' => AuthController::class . '@postLogin']);
-        $router->put('token', ['uses' => AuthController::class . '@putToken']);
+        $router->post('login', ['uses' => 'Auth\AuthController@postLogin']);
+        $router->put('token', ['uses' => 'Auth\AuthController@putToken']);
     });
 
 
 $router->group(['prefix' => 'applications'],
     function () use ($router) {
-        $router->get('for-property/{propertyId}', ApplicationController::class . '@forProperty');
+        $router->get('for-property/{propertyId}', 'ApplicationController@forProperty');
 
-        $router->post('{propertyId}/create-account', ApplicationController::class . '@createAccount');
-        $router->post('{propertyId}/{applicationId}/personal', ApplicationController::class . '@personal');
-        $router->post('{propertyId}/{applicationId}/residential', ApplicationController::class . '@residential');
-        $router->post('{propertyId}/{applicationId}/finish', ApplicationController::class . '@finish');
+        $router->post('{propertyId}/create-account', 'ApplicationController@createAccount');
+        $router->post('{propertyId}/{applicationId}/personal', 'ApplicationController@personal');
+        $router->post('{propertyId}/{applicationId}/residential', 'ApplicationController@residential');
+        $router->post('{propertyId}/{applicationId}/finish', 'ApplicationController@finish');
 
-        $router->post('{propertyId}/{applicationId}/reject', ApplicationController::class . '@reject');
-        $router->post('{propertyId}/{applicationId}/approve', ApplicationController::class . '@approve');
+        $router->post('{propertyId}/{applicationId}/reject', 'ApplicationController@reject');
+        $router->post('{propertyId}/{applicationId}/approve', 'ApplicationController@approve');
 
-        $router->post('{propertyId}/for-user', ApplicationController::class . '@forUser');
-        $router->get('{propertyId}/{applicationId}', ApplicationController::class . '@showApplication');
-
+        $router->post('{propertyId}/for-user', 'ApplicationController@forUser');
+        $router->get('{propertyId}/{applicationId}', 'ApplicationController@showApplication');
     }
 );
