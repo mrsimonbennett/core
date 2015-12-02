474fcf4531690da605ecba97d5bb3d1c79f980c0
jrdn hannah <jrdn@jrdnhannah.co.uk>
2015-12-02 08:02:32 +0000

    [Documents] Add handler for attaching documents

---
+++ src/Property/Commands/AttachDocumentHandler.php
@@ -0,0 +1,39 @@
+<?php namespace FullRent\Core\Property\Commands;
+
+use FullRent\Core\Property\PropertyRepository;
+use FullRent\Core\Property\ValueObjects\DocumentId;
+use FullRent\Core\Property\ValueObjects\PropertyId;
+use SmoothPhp\CommandBus\BaseCommand;
+
+/**
+ * Class AttachDocumentHandler
+ *
+ * @author jrdn hannah <jrdn@jrdnhannah.co.uk>
+ */
+final class AttachDocumentHandler
+{
+    /**
+     * @var PropertyRepository
+     */
+    private $propertyRepository;
+
+    /**
+     * @param PropertyRepository $propertyRepository
+     */
+    public function __construct(PropertyRepository $propertyRepository)
+    {
+        $this->propertyRepository = $propertyRepository;
+    }
+
+    /**
+     * @param AttachDocument $command
+     */
+    public function handle(AttachDocument $command)
+    {
+        $property = $this->propertyRepository->load($command->getPropertyId());
+
+
+
+        $this->propertyRepository->save($property);
+    }
+}
