00298046852e3108cfce8a02054686c8971e8076
jrdn hannah <jrdn@jrdnhannah.co.uk>
2015-12-07 16:25:42 +0000

    [Settings] Create default settings config and settings controller

---
+++ app/Http/Controllers/User/SettingsController.php
@@ -0,0 +1,13 @@
+<?php namespace FullRent\Core\Application\Http\Controllers\User;
+
+use Illuminate\Routing\Controller;
+
+/**
+ * Class SettingsController
+ * @package FullRent\Core\Application\Http\Controllers\User
+ * @author jrdn hannah <jrdn@jrdnhannah.co.uk>
+ */
+final class SettingsController extends Controller
+{
+
+}
