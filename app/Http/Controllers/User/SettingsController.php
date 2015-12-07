168329f35c58575b40ac72db2e4b1259735703d2
jrdn hannah <jrdn@jrdnhannah.co.uk>
2015-12-07 21:57:57 +0000

    [Settings Controller] Create and set default settings when viewing settings

--- app/Http/Controllers/User/SettingsController.php
+++ app/Http/Controllers/User/SettingsController.php
@@ -1,6 +1,12 @@
 <?php namespace FullRent\Core\Application\Http\Controllers\User;
 
+use Log;
 use Illuminate\Routing\Controller;
+use FullRent\Core\QueryBus\QueryBus;
+use FullRent\Core\User\Queries\FindUserSettings;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use FullRent\Core\Application\Http\Helpers\JsonResponse;
+use Symfony\Component\OptionsResolver\Exception\MissingOptionsException;
 
 /**
  * Class SettingsController
@@ -9,18 +15,44 @@ use Illuminate\Routing\Controller;
  */
 final class SettingsController extends Controller
 {
+    /** @var QueryBus */
+    private $query;
+    /** @var JsonResponse */
+    private $json;
+
+    /**
+     * SettingsController constructor.
+     *
+     * @param QueryBus     $query
+     * @param JsonResponse $json
+     */
+    public function __construct(QueryBus $query, JsonResponse $json)
+    {
+        $this->query = $query;
+        $this->json  = $json;
+    }
+
     /**
      * @param string $userId
      * @return array
      */
     public function viewSettings($userId)
     {
-        $config = config('user.settings');
+        $defaults = config('user.settings');
+        $resolver = (new OptionsResolver)
+            ->setDefaults($defaults)
+            ->setRequired(array_merge(array_keys($defaults), ['user_id']))
+            ->setAllowedValues('user_id', $userId);
 
-        foreach ($config as $setting => $value) {
-            $config[$setting] = ['human_name' => trans('user-settings.' . $setting), 'value' => $value];
+        $settings = $resolver->resolve((array) $this->query->query(new FindUserSettings($userId)));
+
+        foreach ($settings as $setting => $value) {
+            $settings[$setting] = [
+                'human_name' => trans('user-settings.' . $setting),
+                'value'      => $value
+            ];
         }
 
-        return $config;
+        return $settings;
     }
 }
