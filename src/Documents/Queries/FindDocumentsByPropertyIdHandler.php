c897597391829c82410e840af7f450af3bc3081d
jrdn hannah <jrdn@jrdnhannah.co.uk>
2015-12-21 13:04:13 +0000

    [Query] Add order clause

--- src/Documents/Queries/FindDocumentsByPropertyIdHandler.php
+++ src/Documents/Queries/FindDocumentsByPropertyIdHandler.php
@@ -34,6 +34,7 @@ final class FindDocumentsByPropertyIdHandler
             ->join('property_documents', 'property_documents.document_id', '=', 'documents.document_id')
             ->where('property_documents.property_id', $query->getPropertyId())
             ->where('documents.deleted_at', NULL)
+            ->orderBy('property_documents.attached_at', 'desc')
             ->limit(10)
             ->get();
 
