30cf5e7ee2cc5dd18ff50b139e7c462496b850c4
Simon Bennett <simon@bennett.im>
2015-12-30 16:56:01 +0000

    [Property] Update

--- app/Http/Controllers/Properties/PropertiesController.php
+++ app/Http/Controllers/Properties/PropertiesController.php
@@ -2,6 +2,9 @@
 namespace FullRent\Core\Application\Http\Controllers\Properties;
 
 use FullRent\Core\Application\Http\Controllers\Controller;
+use FullRent\Core\Application\Http\Requests\Properties\UpdatePropertyHttpRequest;
+use FullRent\Core\Property\Commands\UpdatePropertiesBasicInformation;
+use SmoothPhp\Contracts\CommandBus\CommandBus;
 
 /**
  * Class PropertiesController
@@ -10,6 +13,17 @@ use FullRent\Core\Application\Http\Controllers\Controller;
  */
 final class PropertiesController extends Controller
 {
+    /** @var CommandBus */
+    private $commandBus;
+
+    /**
+     * PropertiesController constructor.
+     * @param CommandBus $commandBus
+     */
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
@@ -25,11 +39,33 @@ final class PropertiesController extends Controller
     {
         return view('dashboard.properties.show');
     }
+
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function edit()
     {
         return view('dashboard.properties.edit');
+    }
+
+    /**
+     * @param $id
+     * @param UpdatePropertyHttpRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update($id, UpdatePropertyHttpRequest $request)
+    {
+        $this->commandBus->execute(new UpdatePropertiesBasicInformation($id,
+                                                                 $request->get('address'),
+                                                                 $request->get('city'),
+                                                                 $request->get('county'),
+                                                                 $request->get('country'),
+                                                                 $request->get('postcode'),
+                                                                 $request->get('bedrooms'),
+                                                                 $request->get('bathrooms'),
+                                                                 $request->get('parking'), ''));
+
+        return redirect('/properties/' . $id);
+
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,6 +22,8 @@ $router->group(['middleware' => 'company'],
                  */
                 $router->get('/properties', ['uses' => 'Properties\PropertiesController@index']);
                 $router->get('/properties/{propertyId}', ['uses' => 'Properties\PropertiesController@show']);
+                $router->put('/properties/{propertyId}', ['uses' => 'Properties\PropertiesController@update']);
+
                 $router->get('/properties/{propertyId}/edit', ['uses' => 'Properties\PropertiesController@edit']);
 
             }
